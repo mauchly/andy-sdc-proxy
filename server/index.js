9231dc4ea1ac27491a6cb082ceecccb6392f9439
Andy Gala <livelifebrands@gmail.com>
2020-04-28 21:08:55 -0700

    test2

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const fullPath = '/Users/jasonjacob/Desktop/seniorProjects/rpt19-front-end-capst
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
