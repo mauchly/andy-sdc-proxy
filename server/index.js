1fbbed7cf2b6f6426b7c93023e95d0487e9c8106
Andy Gala <livelifebrands@gmail.com>
2020-06-24 17:30:06 -0700

    Update urls

--- server/index.js
+++ server/index.js
@@ -2,20 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const fullPath = '/Users/jasonjacob/Desktop/seniorProjects/rpt19-front-end-capstone/jason_FEC_proxy/public/index.html';
+const path = require('path');
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
 app.use(express.static(__dirname + '/../public'));
 
 app.get('/:id', (req, res) => {
-  res.sendFile(fullPath);
+  res.sendFile(path.join(__dirname, '../public', '/index.html'));
 });
 
 app.listen(port, () => {
